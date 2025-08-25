4304671c8f035e1da0e983f56aa1fd522e4b0c2e
faridaselim7 <faridaselim0@gmail.com>
2025-08-25 19:03:19 +0300

    AccountService reviewed

--- AccountService/src/main/java/com/example/demo/apis/resources/AccountTransferRequest.java
+++ AccountService/src/main/java/com/example/demo/apis/resources/AccountTransferRequest.java
@@ -1,18 +1,21 @@
 package com.example.demo.apis.resources;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AccountTransferRequest {
 
-    @NotBlank
+    @NotBlank(message = "From account id is required")
+    @NotNull(message = "From account id is required")
     private String fromAccountId;
 
-    @NotBlank
+    @NotBlank(message = "To account id is required")
+    @NotNull(message = "To account id is required")
     private String toAccountId;
 
-    @Positive
+    @Positive(message = "Amount must be positive")
     private Double amount;
 }
