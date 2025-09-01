34a1504326c8880acbab9ca9ec3fecc2d7782b24
faridaselim7 <faridaselim0@gmail.com>
2025-09-01 12:51:40 +0300

    final account job

--- AccountService/src/main/java/application/jobs/AccountScheduler.java
+++ AccountService/src/main/java/application/jobs/AccountScheduler.java
@@ -1,13 +1,17 @@
 package application.jobs;
 
 import application.enums.Status;
+import application.feign.TransactionDetail;
+import application.feign.TransactionDetailList;
 import application.models.Account;
 import application.repos.AccountRepo;
+import application.feign.TransactionClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 
 @Component
@@ -16,22 +20,39 @@ public class AccountScheduler {
     @Autowired
     private AccountRepo accountRepo;
 
-    @Scheduled(fixedRate = 60 * 60 * 1000)
+    @Autowired
+    private TransactionClient transactionClient;
+
+    @Scheduled(cron = "0 */1 * ? * *") 
     public void markInactiveAccounts() {
         LocalDateTime cutoff = LocalDateTime.now().minusHours(24);
 
         List<Account> accounts = accountRepo.findAll().stream()
                 .filter(acc -> acc.getStatus() == Status.ACTIVE)
-                .filter(acc -> acc.getUpdated_at().isBefore(cutoff))
                 .toList();
 
         accounts.forEach(acc -> {
-            acc.setStatus(Status.INACTIVE);
-            acc.setUpdated_at(LocalDateTime.now());
-            accountRepo.save(acc);
+            try {
+                    TransactionDetailList txList = transactionClient.getTransactionsList(acc.getId());
+
+                    LocalDateTime lastTxTime = txList.getTransactionDetailList().stream()
+                            .map(TransactionDetail::getTimestamp)
+                            .max(Comparator.naturalOrder())
+                            .orElse(null);
+
+                    boolean lastTxOld = lastTxTime.isBefore(cutoff);
+
+                    if (lastTxOld) {
+                        acc.setStatus(Status.INACTIVE);
+                        acc.setUpdated_at(LocalDateTime.now());
+                        accountRepo.save(acc);
+                    }
+            }
+            catch(Exception e){
+                acc.setStatus(Status.INACTIVE);
+                acc.setUpdated_at(LocalDateTime.now());
+                accountRepo.save(acc);
+            }
         });
     }
 }
-
-
-
